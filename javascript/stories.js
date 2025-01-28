c17d36fcf155fc5e0bc080ca5cbec6185fc8b38f
TheRealHenHen <henhenprogrammer@gmail.com>
2025-01-29 01:03:55 +0800

    Remove stories page

--- javascript/stories.js
+++
@@ -1,7 +0,0 @@
-import { toggleDarkMode, initializeDarkMode } from './shared.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    initializeDarkMode();
-    // Make toggleDarkMode available globally
-    window.toggleDarkMode = toggleDarkMode;
-});
